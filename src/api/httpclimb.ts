c37f6d691032aa4e60838e2ac7b1a53b3d92861a
YESCAS139A <24311075@edu.mx>
2026-07-13 15:30:32 -0700

    feat/fe-001_Set-up-the-frontend-project.1

---
+++ src/api/httpclimb.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "https://tuapp.com",
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token && config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
+        }
+    return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+
+api.interceptors.response.use(
+    (response) => response,
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                localStorage.removeItem("token");
+                window.location.href = "/login";
+                }
+        return Promise.reject(error);
+    }
+);
